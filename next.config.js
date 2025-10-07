5c66c759025f0011803325545d08e4d378c36bbf
Tomas <tomaslonnblad@gmail.com>
2025-10-07 14:59:56 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,9 +4,7 @@
 const nextConfig = {
   output: 'export',
   distDir: 'dist',
-  images: {
-  unoptimized: true,
-},
+
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
  

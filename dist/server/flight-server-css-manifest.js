ccdd168048c8f768dfeaefbe093dfded1855155a
TomasLonnblad <tomaslonnblad@gmail.com>
2025-10-07 14:50:49 +0200

    x

---
+++ dist/server/flight-server-css-manifest.js
@@ -0,0 +1,33 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "C:\\Users\\Admin\\OneDrive\\Documents\\Ny mapp\\Ny mapp (10)\\curved-menu-main\\curved-menu-main\\src\\app\\layout.js": [
+      "C:\\Users\\Admin\\OneDrive\\Documents\\Ny mapp\\Ny mapp (10)\\curved-menu-main\\curved-menu-main\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.js\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "C:\\Users\\Admin\\OneDrive\\Documents\\Ny mapp\\Ny mapp (10)\\curved-menu-main\\curved-menu-main\\src\\app\\globals.css",
+      "C:\\Users\\Admin\\OneDrive\\Documents\\Ny mapp\\Ny mapp (10)\\curved-menu-main\\curved-menu-main\\src\\components\\header\\style.module.scss",
+      "C:\\Users\\Admin\\OneDrive\\Documents\\Ny mapp\\Ny mapp (10)\\curved-menu-main\\curved-menu-main\\src\\components\\header\\nav\\style.module.scss",
+      "C:\\Users\\Admin\\OneDrive\\Documents\\Ny mapp\\Ny mapp (10)\\curved-menu-main\\curved-menu-main\\src\\components\\header\\nav\\Link\\style.module.scss",
+      "C:\\Users\\Admin\\OneDrive\\Documents\\Ny mapp\\Ny mapp (10)\\curved-menu-main\\curved-menu-main\\src\\components\\header\\nav\\Curve\\styles.module.scss",
+      "C:\\Users\\Admin\\OneDrive\\Documents\\Ny mapp\\Ny mapp (10)\\curved-menu-main\\curved-menu-main\\src\\components\\header\\nav\\Footer\\style.module.scss"
+    ]
+  },
+  "cssModules": {
+    "C:\\Users\\Admin\\OneDrive\\Documents\\Ny mapp\\Ny mapp (10)\\curved-menu-main\\curved-menu-main\\src\\app\\page": [
+      "C:\\Users\\Admin\\OneDrive\\Documents\\Ny mapp\\Ny mapp (10)\\curved-menu-main\\curved-menu-main\\src\\app\\globals.css",
+      "C:\\Users\\Admin\\OneDrive\\Documents\\Ny mapp\\Ny mapp (10)\\curved-menu-main\\curved-menu-main\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.js\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "C:\\Users\\Admin\\OneDrive\\Documents\\Ny mapp\\Ny mapp (10)\\curved-menu-main\\curved-menu-main\\src\\components\\header\\nav\\Link\\style.module.scss",
+      "C:\\Users\\Admin\\OneDrive\\Documents\\Ny mapp\\Ny mapp (10)\\curved-menu-main\\curved-menu-main\\src\\components\\header\\nav\\Curve\\styles.module.scss",
+      "C:\\Users\\Admin\\OneDrive\\Documents\\Ny mapp\\Ny mapp (10)\\curved-menu-main\\curved-menu-main\\src\\components\\header\\nav\\Footer\\style.module.scss",
+      "C:\\Users\\Admin\\OneDrive\\Documents\\Ny mapp\\Ny mapp (10)\\curved-menu-main\\curved-menu-main\\src\\components\\header\\nav\\style.module.scss",
+      "C:\\Users\\Admin\\OneDrive\\Documents\\Ny mapp\\Ny mapp (10)\\curved-menu-main\\curved-menu-main\\src\\components\\header\\style.module.scss"
+    ],
+    "C:\\Users\\Admin\\OneDrive\\Documents\\Ny mapp\\Ny mapp (10)\\curved-menu-main\\curved-menu-main\\src\\app\\work\\page": [
+      "C:\\Users\\Admin\\OneDrive\\Documents\\Ny mapp\\Ny mapp (10)\\curved-menu-main\\curved-menu-main\\src\\app\\globals.css",
+      "C:\\Users\\Admin\\OneDrive\\Documents\\Ny mapp\\Ny mapp (10)\\curved-menu-main\\curved-menu-main\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.js\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "C:\\Users\\Admin\\OneDrive\\Documents\\Ny mapp\\Ny mapp (10)\\curved-menu-main\\curved-menu-main\\src\\components\\header\\nav\\Link\\style.module.scss",
+      "C:\\Users\\Admin\\OneDrive\\Documents\\Ny mapp\\Ny mapp (10)\\curved-menu-main\\curved-menu-main\\src\\components\\header\\nav\\Curve\\styles.module.scss",
+      "C:\\Users\\Admin\\OneDrive\\Documents\\Ny mapp\\Ny mapp (10)\\curved-menu-main\\curved-menu-main\\src\\components\\header\\nav\\Footer\\style.module.scss",
+      "C:\\Users\\Admin\\OneDrive\\Documents\\Ny mapp\\Ny mapp (10)\\curved-menu-main\\curved-menu-main\\src\\components\\header\\nav\\style.module.scss",
+      "C:\\Users\\Admin\\OneDrive\\Documents\\Ny mapp\\Ny mapp (10)\\curved-menu-main\\curved-menu-main\\src\\components\\header\\style.module.scss"
+    ]
+  }
+}
